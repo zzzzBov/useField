25ff0486001d5781eee818b75a47785c822010ca
Tim Leverett <zzzzbov@gmail.com>
2019-12-31 19:31:49 -0500

    update example

--- example/index.tsx
+++ example/index.tsx
@@ -1,13 +1,18 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { useField } from '../.'
 
 const App = () => {
+  const {
+    set,
+    value
+  } = useField({}, '')
+
   return (
-    <div>
-      <Thing />
-    </div>
+    <form>
+      <input type='text' value={value} onChange={(e) => set(e.currentTarget.value)} />
+    </form>
   );
 };
 
